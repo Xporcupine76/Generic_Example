2e2da6a81704d07b90c039c1c940e5ea3e5c8dd8
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:03:59 +0200

    Add project files.

---
+++ Generic_Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Channels;
+namespace MyStruct
+{
+
+    class Peogram
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list1 = new ArrayList();
+            list1.Add(3);
+            list1.Add(5);
+            list1.Add(2);
+
+            Console.WriteLine("none generic list reults ...");
+            foreach (int x in list1)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
+
